2ece8fd59520600f78d5b368247ab8d818077807
EagleBuck <jdipietro235@gmail.com>
2017-05-16 23:51:57 +0100

    First Commit

---
+++ GameOfThrees-239.py
@@ -0,0 +1,9 @@
+# temp
+
+"""
+Back in middle school, I had a peculiar way of dealing with super boring classes. I would take my handy pocket calculator and play a "Game of Threes". Here's how you play it:
+First, you mash in a random large number to start with. Then, repeatedly do the following:
+If the number is divisible by 3, divide it by 3.
+If it's not, either add 1 or subtract 1 (to make it divisible by 3), then divide it by 3.
+The game stops when you reach "1".
+While the game was originally a race against myself in order to hone quick math reflexes, it also poses an opportunity for some interesting programming challenges. Today, the challenge is to create a program that "plays" the Game of Threes.
