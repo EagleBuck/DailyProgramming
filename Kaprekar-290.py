1c7de12833a0e1819af2717e709506f75fe17737
EagleBuck <jdipietro235@gmail.com>
2017-05-31 13:40:00 -0500

    test

--- Kaprekar-290.py
+++ Kaprekar-290.py
@@ -3,3 +3,26 @@
 # Started 2017-05-22
 
 # https://www.reddit.com/r/dailyprogrammer/comments/5aemnn/20161031_challenge_290_easy_kaprekar_numbers/
+
+'''
+In mathematics, a Kaprekar number for a given base that is a non-negative integer,
+the representation of whose square in that base can be split into two parts that
+add up to the original number again. For instance, 45 is a Kaprekar number,
+because 452 = 2025 and 20+25 = 45. The Kaprekar numbers are named after
+D. R. Kaprekar.
+I was introduced to this after the recent Kaprekar constant challenge.
+For the main challenge we'll only focus on base 10 numbers. For a bonus, see if
+you can make it work in arbitrary bases.
+'''
+
+
+def start():
+    print("start here")
+    userIn = input("Input number")
+    print(userIn)
+    
+
+
+
+
+start()
