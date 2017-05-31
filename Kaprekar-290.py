67d45d0a557bf18b78372e6500684f8c8bc91844
EagleBuck <jdipietro235@gmail.com>
2017-05-31 14:02:06 -0500

    more

--- Kaprekar-290.py
+++ Kaprekar-290.py
@@ -8,7 +8,7 @@
 In mathematics, a Kaprekar number for a given base that is a non-negative integer,
 the representation of whose square in that base can be split into two parts that
 add up to the original number again. For instance, 45 is a Kaprekar number,
-because 452 = 2025 and 20+25 = 45. The Kaprekar numbers are named after
+because 45^2 = 2025 and 20+25 = 45. The Kaprekar numbers are named after
 D. R. Kaprekar.
 I was introduced to this after the recent Kaprekar constant challenge.
 For the main challenge we'll only focus on base 10 numbers. For a bonus, see if
@@ -17,11 +17,20 @@ you can make it work in arbitrary bases.
 
 
 def start():
-    print("start here")
-    userIn = input("Input number")
-    print(userIn)
+    while(True):
+        userIn = input("Input 2 digit number:\n")
+        if(len(userIn) == 2):
+            digitOne = str(userIn)[0]
+            print(digitOne)
+            digitTwo = str(userIn)[1]
+            isKaprekar(digitOne, digitTwo)
+        else:
+            pass
+        
     
 
+def isKaprekar(digitOne, digitTwo):
+    
 
 
 
